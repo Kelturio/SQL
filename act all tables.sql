f7c64e788a176e6e0397995dafd8d4fdf9ff93c1
Searinox <searinox@gmx.de>
2018-09-18 19:44:15 +0200

    akk

--- act all tables.sql
+++ act all tables.sql
@@ -1,15 +1,1 @@
-SELECT TOP (1000) * FROM [ACT].[dbo].[encounter_table] order by id desc
-SELECT TOP (1000) * FROM [ACT].[dbo].[combatant_table] order by id desc
-SELECT TOP (1000) * FROM [ACT].[dbo].[damagetype_table] order by id desc
-SELECT TOP (1000) * FROM [ACT].[dbo].[attacktype_table] order by id desc
-SELECT TOP (1000) * FROM [ACT].[dbo].[swing_table] order by id desc
-SELECT TOP (1000) * FROM [ACT].[dbo].[current_table]
-
-
-
-
-
-
-
-
-
+../../SQL Server Management Studio/act all tables.sql
